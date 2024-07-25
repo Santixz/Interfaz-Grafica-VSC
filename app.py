5d0d7dc7353090a5314a90d135e34074e4e3041b
Moonless'Night <brianariza101mail.com>
2024-07-25 17:15:09 -0500

    lol xd

--- app.py
+++ app.py
@@ -4,3 +4,22 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta_aqui'
+app.config['SESSION TYPE'] = 'filesystem'
+Session(app)
+
+db = mysqlconnector(
+    host="localhost",
+    user="admin"
+    password="admin2024"
+    database="ga_programmers"
+)
+
+cursor = db.cursor()
+
+@app.route('/')
+def index():
+    cursor.execute("SELECT * FROM tbusuarios")
+    datos = cursor.fetchall()
+    return render_template('index.html', tbusuarios=tbusuarios)
+
+@app.route('/login')
